7822335a905345b743cbfee003ac26749d86f22e
Aditya Sharma <aditya.sharma9301@gmail.com>
2022-06-17 15:31:22 +0530

    update supabase config

--- src/config/supabase-client.js
+++ src/config/supabase-client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
+const supabase = createClient('https://alpteondiyihwnramdeq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFscHRlb25kaXlpaHducmFtZGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ3NTY2ODMsImV4cCI6MTk3MDMzMjY4M30.v5rt1ouQOWaEd5E5COepZgDY8pEOTDse5Z3iOv4IK2o');
 
 export default supabase;
